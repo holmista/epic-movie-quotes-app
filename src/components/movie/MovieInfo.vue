e8d1571fb7769261ebfbbd29a49e3fe8be8f40a0
holmista <tornikebuchukuri@redberry.ge>
2022-11-20 17:52:05 +0400

    feat: movies filter functionality

--- src/components/movie/MovieInfo.vue
+++ src/components/movie/MovieInfo.vue
@@ -11,6 +11,7 @@
             class="text-white bg-transparent border-0 w-full"
             type="text"
             placeholder="Search"
+            v-model="search"
           />
         </div>
         <BaseButton class="w-[154px] h-12 bg-[#E31221]" text="Add movie">
@@ -23,7 +24,7 @@
       class="grid grid-cols-1 gap-x-[50px] gap-y-[60px] max-w-[1420px] sm:grid-cols-3"
     >
       <MovieCard
-        v-for="movie in movies"
+        v-for="movie in filteredMovies.value"
         :title="movie.title.en"
         :releaseYear="movie.release_year"
         :image="movie.avatar"
@@ -39,9 +40,36 @@ import SearchIcon from "@/assets/icons/movie/SearchIcon.vue";
 import AddIcon from "@/assets/icons/movie/AddIcon.vue";
 import BaseButton from "@/components/base/BaseButton.vue";
 import useFetch from "@/hooks/useFetch";
-import { onMounted, reactive } from "vue";
+import { onMounted, reactive, ref, watchEffect } from "vue";
 
 const movies = reactive([]);
+let filteredMovies = reactive({ value: [] });
+const search = ref("");
+
+watchEffect(() => {
+  if (search.value) {
+    console.log(search.value);
+    filteredMovies.value = movies.filter((movie) =>
+      movie.title.en.toLowerCase().includes(search.value.toLowerCase())
+    );
+    console.log(filteredMovies);
+  } else {
+    filteredMovies.value = movies;
+  }
+});
+
+// const searchMovies = (e) => {
+//   const searchValue = e.target.value;
+//   if (searchValue) {
+//     console.log("seaarched");
+//     filteredMovies = movies.filter((movie) =>
+//       movie.title.en.toLowerCase().includes(searchValue.toLowerCase())
+//     );
+//     console.log(filteredMovies);
+//   } else {
+//     filteredMovies = movies;
+//   }
+// };
 
 onMounted(async () => {
   const state = await useFetch({
@@ -51,6 +79,5 @@ onMounted(async () => {
   if (state.status.value === 200) {
     movies.push(...state.response.value.movies);
   }
-  //   console.log(state.response.value);
 });
 </script>
